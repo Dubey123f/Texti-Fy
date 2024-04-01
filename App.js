e80aa5d622f8c8c0edea891fff53b6ff1ecf98f4
Ayush Dubey <130337728+Dubey123f@users.noreply.github.com>
2024-04-01 12:16:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+function App() {
+  const [mode, setMode] = useState('light'); 
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+  return (
+    <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    </div>
+    </> 
+  );
+  
+ 
+};
+
+export default App;
